22d5d93d7469f2a7356351eb15d4d6970d916cef
Logan Levin <loganjlevin@gmail.com>
2023-02-18 17:33:49 -0800

    added request methods to documentation.html, added basic functionality of all request methods

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // import express and morgan packages
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
 
 // assign express to app object
 const app = express();
 
-// list of top movie objects
-let topMovies = [
+// use morgan logging package
+app.use(morgan('common'));
+
+// use body parser package
+app.use(bodyParser.json());
+
+// send static files from public folder when requested
+app.use(express.static('public'));
+
+// list of movie objects
+let movies = [
   {
     title: 'Citizen Kane',
     director: 'Orson Welles',
@@ -49,21 +60,65 @@ let topMovies = [
   },
 ];
 
-// use morgan logging package
-app.use(morgan('common'));
-
-// display movies as json when requested at /movies
+// get a list of all movies
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
 });
 
-// send a default text response when requested at /
-app.get('/', (req, res) => {
-  res.send('Default textual response of my choosing.');
+// get data about a movie by title
+app.get('/movies/:title', (req, res) => {
+  res.json(
+    movies.find((movie) => {
+      return movie.title === req.params.title;
+    })
+  );
 });
 
-// send static files from public folder when requested
-app.use(express.static('public'));
+// get data about a genre by name
+app.get('/genres/:title', (req, res) => {
+  res.send('Successful GET request returning data on a genre.');
+});
+
+// get data about a director by name
+app.get('/directors/:name', (req, res) => {
+  res.send('Successful GET request returning data on a director.');
+});
+
+// post a new user's data
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.username) {
+    const message = "Missing 'username' in request body";
+    res.status(400).send(message);
+  } else {
+    res.send('Successful POST request returning json object of user with id');
+  }
+});
+
+// Update a user's username by id
+app.put('/users/:id/:username', (req, res) => {
+  res.send('Successful PUT request indicating the username was updated');
+});
+
+// Add a movie to a user's list of favorites
+app.put('/users/:id/favorites/:title', (req, res) => {
+  res.send(
+    'Successful PUT request indicating the movie was added to favorites'
+  );
+});
+
+// Remove a movie from a user's list of favorites
+app.delete('/users/:id/favorites/:title', (req, res) => {
+  res.send(
+    'Successful DELETE request indicating the movie was removed from favorites'
+  );
+});
+
+// Deregister an existing user
+app.delete('/users/:id', (req, res) => {
+  res.send('Successful DELETE request indicating the user was deregistered');
+});
 
 // error handling in case something goes wrong
 app.use((err, req, res, next) => {
