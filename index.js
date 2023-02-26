b6c8fb6105dc835d9b8d4d4c64390bc23f5145e5
Logan Levin <loganjlevin@gmail.com>
2023-02-26 13:10:40 -0800

    cors allow all

--- index.js
+++ index.js
@@ -30,25 +30,30 @@ const passport = require('passport');
 require('./passport.js');
 
 // use cors Cross-Origin Resource Sharing
-let allowedOrigins = ['*', 'http://localhost:1234'];
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins[0] === '*') {
-        return callback(null, true);
-      }
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn't found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn't allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
+    origin: '*',
   })
 );
+//let allowedOrigins = ['*', 'http://localhost:1234'];
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins[0] === '*') {
+//         return callback(null, true);
+//       }
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn't found on the list of allowed origins
+//         let message =
+//           "The CORS policy for this application doesn't allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 // use morgan logging package
 app.use(morgan('common'));
